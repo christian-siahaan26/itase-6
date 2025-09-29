5d653abd39075bf2b918ac452ef560dd632d76ea
ToDoList <christian123star@gmail.com>
2025-09-30 01:41:34 +0700

    Fix vercel.json

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,31 +1,24 @@
 import { Express } from "express";
+import fs from "fs";
+import path from "path";
 import swaggerUi from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerUICss =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 
 export function setupSwagger(app: Express) {
-  const swaggerOptions: swaggerJSDoc.Options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "API Documentation for ITASE Project",
-        version: "1.0.0",
-        description:
-          "Dokumentasi REST API untuk proyek Predictive Lead Scoring",
-      },
-      servers: [
-        {
-          url: "https://itase-6.vercel.app", // URL base proyek Anda
-        },
-      ],
-    },
-    // Pastikan path ini menunjuk ke file rute Anda yang berisi komentar JSDoc
-    apis: ["./src/routes/*.ts"],
-  };
+  const swaggerFilePath = path.resolve(__dirname, "../docs/swagger.json");
+  const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, "utf-8"));
+  // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+      customCss:
+        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+      customCssUrl: swaggerUICss,
+    })
+  );
 
-  const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
-  // Gunakan setup default tanpa kustomisasi CSS/JS untuk kesederhanaan di Vercel
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-  console.log("📚 Swagger docs available at /api-docs");
+  console.log("📚 Swagger docs available at http://localhost:3000/api-docs");
 }
